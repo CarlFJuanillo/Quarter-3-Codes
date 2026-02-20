5cc9d6e1525d7d6331db13f3c7de4d7a911859a2
CarlFJuanillo <carlodie.juanillo@smc.pshs.edu.ph>
2026-02-20 14:50:29 +0800

    Add student record input and display functionality

---
+++ Juanillo_Q3_FA06.py
@@ -0,0 +1,10 @@
+student = {}
+
+name = input("What is your name? : ")
+age = int(input("What is your age? : "))
+subject = input("What is your favourite subject? : ")
+
+print("\nStudent Record : ")
+print("Name : ", name)
+print("Age : ", age)
+print("Favourite Subject : ", subject)
